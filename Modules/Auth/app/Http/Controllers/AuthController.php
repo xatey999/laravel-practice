970acb6270d7f6152bac1fd28c036c574a1d142d
xatey999 <ujjwal999adhikari@gmail.com>
2026-03-15 23:23:54 +0545

    feat(auth): add logout functionality with token validation and response handling

--- Modules/Auth/app/Http/Controllers/AuthController.php
+++ Modules/Auth/app/Http/Controllers/AuthController.php
@@ -5,12 +5,12 @@ namespace Modules\Auth\Http\Controllers;
 use App\Actions\CreateUserAction;
 use App\Http\Controllers\Api\ApiBaseController;
 use App\DataTransferObjects\RegisterFormPayload;
-use Illuminate\Support\Facades\Auth;
 use Modules\Auth\Http\Requests\RegisterRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends ApiBaseController
@@ -39,11 +39,11 @@ class AuthController extends ApiBaseController
 
         $user = User::where('email', $validated['email'])->first();
 
-        if(!$user) {
+        if (!$user) {
             return $this->sendError("Invalid email, user doesn't exists!", Response::HTTP_NOT_FOUND);
         }
 
-        if(!Hash::check($validated['password'], $user->password)) {
+        if (!Hash::check($validated['password'], $user->password)) {
             return $this->sendError("Unauthorized!", Response::HTTP_UNAUTHORIZED);
         }
 
@@ -57,5 +57,25 @@ class AuthController extends ApiBaseController
             'Login successful',
             Response::HTTP_OK
         );
+    }
+
+    public function logout(Request $request)
+    {
+        $token = $request->bearerToken();
+
+        if ($token === null) {
+            return $this->sendError("Bearer token not provided!", Response::HTTP_BAD_REQUEST);
+        }
+
+        $tokenId = Str::before($token, '|');
+
+        $user = auth()->user();
+
+        if ($user) {
+            $user->tokens()->where('id', $tokenId)->delete();
+            return $this->sendResponse([], "Logged out Successfully!");
+        }
+
+        return $this->sendError('User not authenticated', Response::HTTP_UNAUTHORIZED);
     }
 }
--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -7,5 +7,7 @@ Route::prefix('v1')->group(function () {
     Route::controller(AuthController::class)->group(function () {
         Route::post('register', 'register')->name('user.register');
         Route::post('login', 'login')->name('user.login');
-    });
+        Route::post('logout', 'logout')->name('user.logout')
+            ->middleware('auth:sanctum');
+    })->middleware('throttle:api');
 });
